a233e1b2fdfa67a1a1a418d08e04243c0e648e49
a6tij <abdullahalzoubi1567@gmail.com>
2023-09-06 19:35:41 +0300

    day6

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+# Part 1
+person = {
+
+    "name" : "Mahdi",
+    "age" : "18",
+    "hobbies" : ["Programing", "Weapon duel", "Photography"],
+}
+print(person['name'])
+print(person['age'])
+
+# Part 2
+person['age'] = '25'
+person['country'] = 'Kuwait'
+print(person)
+print(len(person))
+
+# Part 3
+person["hobbies"].append('hacking')
+
+def check_hobbies(person):
+    if len(person["hobbies"]) >= 3:
+        print("WOW YOU ARE AMAZING")
+
+check_hobbies(person)
